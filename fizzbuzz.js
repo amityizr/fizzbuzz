1a60023d2848f0e33c3306c37dd08eed4af49754
Amit Yizraeli <amit-yiz@noal.org.il>
2020-07-16 12:55:18 +0300

    small css stuff

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,7 @@ for (let i=1; i<101; i++) {
     if ((i%3==0)&&(i%5==0)) { 
         console.log("FizzBuzz");
         document.getElementById('answer').innerHTML +=  "<div class='number'>" +
-                                                        "<img id='fizz'src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZ6ZlSPwwQDSEOHOqkYKqJoRwBnreL9WZNlA&usqp=CAU'/>" +
+                                                        "<img id='fizz'src='https://www.seekpng.com/png/full/194-1942788_birthday-bash-big-bubble-pop-fizz-icon-activision.png'/>" +
                                                         "FizzBuzz" +
                                                         "<img id='buzz' src='https://www.khwiki.com/images/thumb/b/b1/Buzz_Lightyear_KHIII.png/315px-Buzz_Lightyear_KHIII.png'/>" +
                                                         "</div>";
@@ -12,7 +12,7 @@ for (let i=1; i<101; i++) {
         console.log("Fizz");
         document.getElementById('answer').innerHTML +=  "<div class='number'>"+
                                                         "Fizz" +
-                                                        "<img id='fizz'src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZ6ZlSPwwQDSEOHOqkYKqJoRwBnreL9WZNlA&usqp=CAU'/>" +
+                                                        "<img id='fizz'src='https://www.seekpng.com/png/full/194-1942788_birthday-bash-big-bubble-pop-fizz-icon-activision.png'/>" +
                                                         "</div>";
     }
     else if (i % 5 == 0) { 
