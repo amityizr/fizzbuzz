9d61bc7c03c2485c7f7d59b9f685d68f5e924525
Amit Yizraeli <amit-yiz@noal.org.il>
2020-07-16 12:45:49 +0300

    css grid & innerHTML

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,34 @@
 for (let i=1; i<101; i++) {
+   
     if ((i%3==0)&&(i%5==0)) { 
         console.log("FizzBuzz");
-        document.getElementById('answer').innerHTML += "<br> FizzBuzz";
+        document.getElementById('answer').innerHTML +=  "<div class='number'>" +
+                                                        "<img id='fizz'src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZ6ZlSPwwQDSEOHOqkYKqJoRwBnreL9WZNlA&usqp=CAU'/>" +
+                                                        "FizzBuzz" +
+                                                        "<img id='buzz' src='https://www.khwiki.com/images/thumb/b/b1/Buzz_Lightyear_KHIII.png/315px-Buzz_Lightyear_KHIII.png'/>" +
+                                                        "</div>";
     }
     else if (i % 3 == 0) {
         console.log("Fizz");
-        document.getElementById('answer').innerHTML += "<br> Fizz";
+        document.getElementById('answer').innerHTML +=  "<div class='number'>"+
+                                                        "Fizz" +
+                                                        "<img id='fizz'src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZ6ZlSPwwQDSEOHOqkYKqJoRwBnreL9WZNlA&usqp=CAU'/>" +
+                                                        "</div>";
     }
-    else if (i % 3 == 0) { 
+    else if (i % 5 == 0) { 
         console.log("Buzz");
-        document.getElementById('answer').innerHTML += "<br> Buzz";
+        document.getElementById('answer').innerHTML +=  "<div class='number'>" + 
+                                                        "Buzz" +
+                                                        "<img id='buzz' src='https://www.khwiki.com/images/thumb/b/b1/Buzz_Lightyear_KHIII.png/315px-Buzz_Lightyear_KHIII.png'/>" + 
+                                                        "</div>";
     }
     else {
         console.log(i)
-        document.getElementById('answer').innerHTML += "<br>"
-        document.getElementById('answer').innerHTML += i.toString();
+        document.getElementById('answer').innerHTML +=  "<div class='number'>" + 
+                                                        i.toString() + 
+                                                        "</div>";
     }
+
 }
 
 
